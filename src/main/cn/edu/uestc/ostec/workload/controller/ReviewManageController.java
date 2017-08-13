fb32bbe838258e9a7e36b4158fc01e94528b58e9
张健顺 <1224522500@qq.com>
2017-08-13 14:19:13 +0800

    修复不能添加类目的问题

--- src/main/cn/edu/uestc/ostec/workload/controller/ReviewManageController.java
+++ src/main/cn/edu/uestc/ostec/workload/controller/ReviewManageController.java
@@ -7,7 +7,6 @@
  */
 package cn.edu.uestc.ostec.workload.controller;
 
-import org.junit.runner.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
